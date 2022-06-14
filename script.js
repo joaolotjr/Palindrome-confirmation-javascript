84c083ca73da74e5a40c6d55ae916635e3192a06
joaolotjr <joaolotjr@gmail.com>
2022-06-14 12:32:47 -0300

    solution 2

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// solution 1
+//solution 1
 function verificationPalindrome(string) {
-  if (!string) return
+  if (!string) return 'non-existent string'
 
   return string.split('').reverse().join('') === string
 }
 
-console.log(verificationPalindrome('level'))
+// console.log(verificationPalindrome('level'))
+
+// solution 2
+
+function verificationPalindrome2(string) {
+  if (!string) return 'non-existent string'
+
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false
+    }
+  }
+  return true
+}
+
+console.log(verificationPalindrome2('level'))
